e7086eda845b5d4e0d6a0b2368385363eb1e34cb
mohamadrahdan <m.rahdan7@gmail.com>
2025-05-05 23:08:28 +0330

    SentinelDownloader
    Integrate SentinelDownloader with main pipeline for real image acquisition

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from step1_download_images import ImageDownloader
+from step1a_simulated_downloader import ImageDownloader
 from step2_preprocess_images import ImagePreprocessor
 from step3_save_ready_images import ImageSaver
 from config_sentinelhub import get_config, test_config
+from step1b_sentinel_downloader import SentinelDownloader
+
 
 if __name__ == "__main__":
     # Step 1: Download images
@@ -19,4 +21,12 @@ if __name__ == "__main__":
     config = get_config(profile_name="landsat-pipeline")
     test_config(config)
 
+    # Define bounding box [min_lon, min_lat, max_lon, max_lat]
+    bbox = [51.5, 32.5, 51.6, 32.6]  # an small area around the Isfahan city
 
+    # Define date range
+    time_interval = ("2023-08-01", "2023-08-10")
+
+    # Create downloader instance and start download
+    downloader = SentinelDownloader(bbox=bbox, time_interval=time_interval)
+    downloader.download()
